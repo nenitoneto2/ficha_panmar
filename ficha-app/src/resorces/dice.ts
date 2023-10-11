16930937d6fdbdd2b53c792f99f48b3ff5a0517a
felipe assis <felipe.assis.2002@gmail.com>
2023-10-11 11:27:13 -0300

    Adding dice

---
+++ ficha-app/src/resorces/dice.ts
@@ -0,0 +1,15 @@
+function rollDice(numDice: number, numFaces: number, modifier: number): number {
+    let total: number = 0;
+  
+    for (let i = 0; i < numDice; i++) {
+      // Roll the die and add the result to the total
+      total += Math.floor(Math.random() * numFaces) + 1;
+    }
+  
+    // Add the modifier to the total
+    total += modifier;
+  
+    return total;
+  }
+
+  export default rollDice;
