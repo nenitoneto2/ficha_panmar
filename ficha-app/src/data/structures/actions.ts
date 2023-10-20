92505f7956b181214e820199af31b3083a6ca7bb
felipe assis <felipe.assis.2002@gmail.com>
2023-10-20 15:51:44 -0300

    finishng magic structure

---
+++ ficha-app/src/data/structures/actions.ts
@@ -0,0 +1,13 @@
+enum ActionSpeed{
+    FullRound,
+    CompleteAction,
+    PartialAction,
+    BonusAction,
+    FreeAction,
+    CriticalAction,
+    Reaction,
+    Concatenated,
+    Passive
+}
+
+export default ActionSpeed
