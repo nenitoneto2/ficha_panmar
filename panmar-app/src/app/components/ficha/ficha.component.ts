0883e824eea82b915a6045f774af6c2acf4cc75f
nenitoneto2 <ivoveneto@gmail.com>
2023-11-07 20:03:11 -0300

    feat(ficha): adiciona pagina base para ficha

--- panmar-app/src/app/components/ficha/ficha.component.ts
+++ panmar-app/src/app/components/ficha/ficha.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-ficha',
   templateUrl: './ficha.component.html',
   styleUrls: ['./ficha.component.scss']
 })
 export class FichaComponent {
-
+  cavalo:number =0;
+  addnumber() {
+    this.cavalo++
+  }
 }
