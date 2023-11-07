a60f48b5b9b61fde048b6bc0ce8ba20bc404da67
nenitoneto2 <ivoveneto@gmail.com>
2023-11-07 20:03:11 -0300

    feat(ficha): adiciona pagina base para ficha

--- panmar-app/src/app/app-routing.module.ts
+++ panmar-app/src/app/app-routing.module.ts
@@ -5,10 +5,12 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { FichaComponent } from './components/ficha/ficha.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+  { path: '', redirectTo: '/ficha', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
+  { path: 'ficha', component: FichaComponent },
   { path: 'register-user', component: SignUpComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
