6faa844b7327a9bd4352da934d9f61ab5cdc3b1f
feassis <felipe.assis.2002@gmail.com>
2023-12-06 10:58:34 -0300

    Crud Implementation

--- panmar-app/src/data/db/characterDBService.ts
+++
@@ -1,5 +0,0 @@
-const CharacterModel = require("../../../data/db/CharacterDBObject")
-CharacterModel.methods(['get', 'post', 'put', 'delete'])
-CharacterModel.updateOptions({new: true, runValidators: true})
-
-module.exports = CharacterModel
