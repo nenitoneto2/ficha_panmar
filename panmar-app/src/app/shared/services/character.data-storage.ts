4d215ae1f54c2ae1c404206abec7c496eafac783
Felipe Assis <felipe.assis.2002@gmail.com>
2024-05-11 10:50:06 -0300

    adding auth param

--- panmar-app/src/app/shared/services/character.data-storage.ts
+++ panmar-app/src/app/shared/services/character.data-storage.ts
@@ -1,21 +1,24 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { CharacterService } from "./character.service";
 import { Character } from "../classes/character/character";
 import { tap } from "rxjs";
+import { AuthService } from "./auth.service";
 
-const path: string = 'https://pan-mar-default-rtdb.firebaseio.com/characters/'
+const path: string = 'https://pan-mar-default-rtdb.firebaseio.com/users/'
 
 @Injectable()
 export class CharacterDataStorageService{
-    constructor(private http: HttpClient, private charService: CharacterService){
+    constructor(private http: HttpClient, private charService: CharacterService, private authService: AuthService){
 
     }
 
     storeCharacters(){
         const characters = this.charService.GetCharacters();
         this.http.put(
-            path + 'characters.json', characters
+            path + this.authService.userData.uid +"/characters/"+ 'characters.json', characters, {
+                params: new HttpParams().set('auth', this.authService.userData.uid)
+            }
         ).subscribe(response => {
             console.log(response)
         })
@@ -23,7 +26,9 @@ export class CharacterDataStorageService{
 
     fetchCharacter(){
         return this.http.get<Character[]>(
-            path + 'characters.json'
+            path + this.authService.userData.uid +"/characters/"+  'characters.json', {
+                params: new HttpParams().set('auth', this.authService.userData.uid)
+            }
         ).pipe(
             tap(characters => {
             this.charService.SetCharacters(characters)
